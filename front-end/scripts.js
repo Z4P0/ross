3804e205287401523bb744c52363808ed420b831
luis rosario <lrosario101@gmail.com>
2016-02-24 01:37:09 -0500

    added timers and some visual feedback

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,24 +1,86 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var xhr = require('xhr');
+var Timer = require('timer.js');
 
 window.onload = function() {
+
   // loadup images
   var images = [];
   var req = xhr('/json', function(err, res, body) {
     images = JSON.parse(body);
   });
 
+
   // DOM refs
   var startBtn = document.querySelector('#start-app'),
-      img = document.querySelector('#img');
+      img = document.querySelector('#img'),
+      timeFeedback = document.querySelector('#time');
+
+
+  // warm-up intervals
+  var intervals = [
+        10, 10, 10, 10, 10,   // 5 images x 10 sec
+        30, 30, 30, 30, 30,   // 5 images x 30 sec
+        60, 60, 60,           // 3 images x 1 min
+        180, 180, 180         // 3 images x 3 min
+      ],
+      intervalIndex = 0;  // track where we are in the intervals[]
+
+
+  /**
+   * returns a random image from the images array
+   * @return {string} image path
+   */
+  function randomImage() {
+    return images[Math.floor(Math.random() * images.length)];
+  }
+
+  /**
+   * update <span> text to show the remaining time
+   */
+  function feedback(text) {
+    timeFeedback.textContent = text;
+  }
+
+
+  // timer
+  var timer = new Timer({
+    tick    : 1,
+    ontick  : function(sec) {
+      feedback(Math.floor(sec/1000) + 1);
+    },
+    onstart : function() {
+      // fade to black?
+      feedback(intervals[intervalIndex] + ' seconds');
+    },
+    // onstop  : function() { },
+    // onpause : function() { console.log('timer set on pause') },
+    onend   : function() {
+      intervalIndex++;
+      img.src = randomImage();
+      if (intervalIndex >= intervals.length - 1) {
+        // console.log('last warm-up image');
+        this.start(intervals[intervalIndex]).on('end', function() {
+          feedback('--')
+          img.src = randomImage();
+        });
+      } else {
+        this.start(intervals[intervalIndex]);
+      }
+    }
+  });
+
+
 
   // setup start button
   startBtn.onclick = function(e) {
-    img.src = images[Math.floor(Math.random() * images.length)];
+    img.src = randomImage();
+    timer.start(intervals[intervalIndex]);
+    this.style.display = 'none';
   }
 }
 
-},{"xhr":8}],2:[function(require,module,exports){
+},{"timer.js":7,"xhr":9}],2:[function(require,module,exports){
 var isFunction = require('is-function')
 
 module.exports = forEach
@@ -149,7 +211,144 @@ module.exports = function (headers) {
 
   return result
 }
-},{"for-each":2,"trim":7}],7:[function(require,module,exports){
+},{"for-each":2,"trim":8}],7:[function(require,module,exports){
+(function (root, factory) {
+  'use strict'
+  if (typeof define === 'function' && define.amd) define([], factory)
+  else if (typeof exports === 'object') module.exports = factory()
+  else root.Timer = factory()
+}(this, function () {
+  'use strict'
+
+  var defaultOptions = {
+    tick    : 1,
+    onstart : null,
+    ontick  : null,
+    onpause : null,
+    onstop  : null,
+    onend   : null
+  }
+
+  var Timer = function (options) {
+    if (!(this instanceof Timer)) return new Timer(options)
+    this._ = {
+      id       : +new Date,
+      options  : {},
+      duration : 0,
+      status   : 'initialized',
+      start    : 0,
+      measures : []
+    }
+    for (var prop in defaultOptions) this._.options[prop] = defaultOptions[prop]
+    this.options(options)
+  }
+
+  Timer.prototype.start = function (duration) {
+    if (!+duration && !this._.duration) return this
+    duration && (duration *= 1000)
+    if (this._.timeout && this._.status === 'started') return this
+    this._.duration = duration || this._.duration
+    this._.timeout = setTimeout(end.bind(this), this._.duration)
+    if (typeof this._.options.ontick === 'function')
+      this._.interval = setInterval(function () {
+        trigger.call(this, 'ontick', this.getDuration())
+      }.bind(this), +this._.options.tick * 1000)
+    this._.start = +new Date
+    this._.status = 'started'
+    trigger.call(this, 'onstart', this.getDuration())
+    return this
+  }
+
+  Timer.prototype.pause = function () {
+    if (this._.status !== 'started') return this
+    this._.duration -= (+new Date - this._.start)
+    clear.call(this, false)
+    this._.status = 'paused'
+    trigger.call(this, 'onpause')
+    return this
+  }
+
+  Timer.prototype.stop = function () {
+    if (!/started|paused/.test(this._.status)) return this
+    clear.call(this, true)
+    this._.status = 'stopped'
+    trigger.call(this, 'onstop')
+    return this
+  }
+
+  Timer.prototype.getDuration = function () {
+    if (this._.status === 'started')
+      return this._.duration - (+new Date - this._.start)
+    if (this._.status === 'paused') return this._.duration
+    return 0
+  }
+
+  Timer.prototype.getStatus = function () {
+    return this._.status
+  }
+
+  Timer.prototype.options = function (option, value) {
+    if (option && value) this._.options[option] = value
+    if (!value && typeof option === 'object')
+      for (var prop in option)
+        if (this._.options.hasOwnProperty(prop))
+          this._.options[prop] = option[prop]
+    return this
+  }
+
+  Timer.prototype.on = function (option, value) {
+    if (typeof option !== 'string' || typeof value !== 'function') return this
+    if (!(/^on/).test(option))
+      option = 'on' + option
+    if (this._.options.hasOwnProperty(option))
+      this._.options[option] = value
+    return this
+  }
+
+  Timer.prototype.off = function (option) {
+    if (typeof option !== 'string') return this
+    option = option.toLowerCase()
+    if (option === 'all') {
+      this._.options = defaultOptions
+      return this
+    }
+    if (!(/^on/).test(option)) option = 'on' + option
+    if (this._.options.hasOwnProperty(option))
+      this._.options[option] = defaultOptions[option]
+    return this
+  }
+
+  Timer.prototype.measureStart = function (label) {
+    this._.measures[label || ''] = +new Date
+    return this
+  }
+
+  Timer.prototype.measureStop = function (label) {
+    return +new Date - this._.measures[label || '']
+  }
+
+  function end () {
+    clear.call(this)
+    this._.status = 'stopped'
+    trigger.call(this, 'onend')
+  }
+
+  function trigger (event) {
+    var callback = this._.options[event],
+      args = [].slice.call(arguments, 1)
+    typeof callback === 'function' && callback.apply(this, args)
+  }
+
+  function clear (clearDuration) {
+    clearTimeout(this._.timeout)
+    clearInterval(this._.interval)
+    if (clearDuration === true) this._.duration = 0
+  }
+
+  return Timer
+}))
+
+},{}],8:[function(require,module,exports){
 
 exports = module.exports = trim;
 
@@ -165,7 +364,7 @@ exports.right = function(str){
   return str.replace(/\s*$/, '');
 };
 
-},{}],8:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 "use strict";
 var window = require("global/window")
 var once = require("once")
@@ -386,7 +585,7 @@ function _createXHR(options) {
 
 function noop() {}
 
-},{"global/window":3,"is-function":4,"once":5,"parse-headers":6,"xtend":9}],9:[function(require,module,exports){
+},{"global/window":3,"is-function":4,"once":5,"parse-headers":6,"xtend":10}],10:[function(require,module,exports){
 module.exports = extend
 
 var hasOwnProperty = Object.prototype.hasOwnProperty;
--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,18 +1,80 @@
 var xhr = require('xhr');
+var Timer = require('timer.js');
 
 window.onload = function() {
+
   // loadup images
   var images = [];
   var req = xhr('/json', function(err, res, body) {
     images = JSON.parse(body);
   });
 
+
   // DOM refs
   var startBtn = document.querySelector('#start-app'),
-      img = document.querySelector('#img');
+      img = document.querySelector('#img'),
+      timeFeedback = document.querySelector('#time');
+
+
+  // warm-up intervals
+  var intervals = [
+        10, 10, 10, 10, 10,   // 5 images x 10 sec
+        30, 30, 30, 30, 30,   // 5 images x 30 sec
+        60, 60, 60,           // 3 images x 1 min
+        180, 180, 180         // 3 images x 3 min
+      ],
+      intervalIndex = 0;  // track where we are in the intervals[]
+
+
+  /**
+   * returns a random image from the images array
+   * @return {string} image path
+   */
+  function randomImage() {
+    return images[Math.floor(Math.random() * images.length)];
+  }
+
+  /**
+   * update <span> text to show the remaining time
+   */
+  function feedback(text) {
+    timeFeedback.textContent = text;
+  }
+
+
+  // timer setup
+  var timer = new Timer({
+    tick    : 1,
+    ontick  : function(sec) {
+      feedback(Math.floor(sec/1000) + 1);
+    },
+    onstart : function() {
+      // fade to black?
+      feedback(intervals[intervalIndex] + ' seconds');
+    },
+    // onstop  : function() { },
+    // onpause : function() { console.log('timer set on pause') },
+    onend   : function() {
+      intervalIndex++;
+      img.src = randomImage();
+      if (intervalIndex >= intervals.length - 1) {
+        // console.log('last warm-up image');
+        this.start(intervals[intervalIndex]).on('end', function() {
+          feedback('--')
+          img.src = randomImage();
+        });
+      } else {
+        this.start(intervals[intervalIndex]);
+      }
+    }
+  });
+
+
 
   // setup start button
   startBtn.onclick = function(e) {
-    img.src = images[Math.floor(Math.random() * images.length)];
+    img.src = randomImage();
+    timer.start(intervals[intervalIndex]);
+    this.style.display = 'none';
   }
 }
