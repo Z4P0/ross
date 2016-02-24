97a2f0b36a3fbb995f60c8c1a9f87d99e928b31a
luis rosario <lrosario101@gmail.com>
2016-02-23 22:04:58 -0500

    express server is serving images json and front-end

--- index.js
+++ index.js
@@ -2,7 +2,28 @@
 
 var fs = require('fs');
 var path = require('path');
+var open = require('open');
+var express = require('express');
 
+
+// gather images
 var images = fs.readdirSync('./').filter(function(f){
   return f && f[0] != '.'; // Ignore hidden files
+});
+
+
+// start express to serve up the front-end
+var app = express();
+
+// serve the front-end folder
+app.use('/', express.static(path.join(__dirname, 'front-end')));
+// send json of images in the invoked directory
+app.get('/images', function(req, res) {
+  res.send(images);
+});
+
+// start the server
+app.listen(3000, function () {
+  console.log('draw away. ctrl+c to stop');
+  open('http://localhost:3000/');
 });
