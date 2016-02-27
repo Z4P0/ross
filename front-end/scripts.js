f20f328cc03a22c2ff017066901d084e0050a1d8
luis rosario <lrosario101@gmail.com>
2016-02-26 19:53:53 -0500

    added a pause button. changed the feedback() function

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -13,6 +13,7 @@ window.onload = function() {
 
   // DOM refs
   var startBtn = document.querySelector('#start-app'),
+      pauseBtn = document.querySelector('#pause-app'),
       img = document.querySelector('#img'),
       timeFeedback = document.querySelector('#time');
 
@@ -26,6 +27,8 @@ window.onload = function() {
       ],
       intervalIndex = 0;  // track where we are in the intervals[]
 
+  // check if we're paused or not
+  var paused = false;
 
   /**
    * returns a random image from the images array
@@ -38,20 +41,20 @@ window.onload = function() {
   /**
    * update <span> text to show the remaining time
    */
-  function feedback(text) {
-    timeFeedback.textContent = text;
+  function feedback(text, domNode) {
+    domNode.textContent = text;
   }
 
 
-  // timer
+  // timer setup
   var timer = new Timer({
     tick    : 1,
     ontick  : function(sec) {
-      feedback(Math.floor(sec/1000) + 1);
+      feedback(Math.floor(sec/1000) + 1, timeFeedback);
     },
     onstart : function() {
       // fade to black?
-      feedback(intervals[intervalIndex] + ' seconds');
+      feedback(intervals[intervalIndex] + ' seconds', timeFeedback);
     },
     // onstop  : function() { },
     // onpause : function() { console.log('timer set on pause') },
@@ -61,7 +64,7 @@ window.onload = function() {
       if (intervalIndex >= intervals.length - 1) {
         // console.log('last warm-up image');
         this.start(intervals[intervalIndex]).on('end', function() {
-          feedback('--')
+          feedback('--', timeFeedback);
           img.src = randomImage();
         });
       } else {
@@ -77,6 +80,20 @@ window.onload = function() {
     img.src = randomImage();
     timer.start(intervals[intervalIndex]);
     this.style.display = 'none';
+  }
+
+  // setup pause button
+  // ----------------------------------------
+  pauseBtn.onclick = function(e) {
+    if (paused) {
+      timer.start();
+      paused = false;
+      feedback('Pause', pauseBtn);
+    } else {
+      timer.pause();
+      paused = true;
+      feedback('Resume', pauseBtn);
+    }
   }
 }
 
--- front-end/src/app.js
+++ front-end/src/app.js
@@ -12,6 +12,7 @@ window.onload = function() {
 
   // DOM refs
   var startBtn = document.querySelector('#start-app'),
+      pauseBtn = document.querySelector('#pause-app'),
       img = document.querySelector('#img'),
       timeFeedback = document.querySelector('#time');
 
@@ -25,6 +26,8 @@ window.onload = function() {
       ],
       intervalIndex = 0;  // track where we are in the intervals[]
 
+  // check if we're paused or not
+  var paused = false;
 
   /**
    * returns a random image from the images array
@@ -37,8 +40,8 @@ window.onload = function() {
   /**
    * update <span> text to show the remaining time
    */
-  function feedback(text) {
-    timeFeedback.textContent = text;
+  function feedback(text, domNode) {
+    domNode.textContent = text;
   }
 
 
@@ -46,11 +49,11 @@ window.onload = function() {
   var timer = new Timer({
     tick    : 1,
     ontick  : function(sec) {
-      feedback(Math.floor(sec/1000) + 1);
+      feedback(Math.floor(sec/1000) + 1, timeFeedback);
     },
     onstart : function() {
       // fade to black?
-      feedback(intervals[intervalIndex] + ' seconds');
+      feedback(intervals[intervalIndex] + ' seconds', timeFeedback);
     },
     // onstop  : function() { },
     // onpause : function() { console.log('timer set on pause') },
@@ -60,7 +63,7 @@ window.onload = function() {
       if (intervalIndex >= intervals.length - 1) {
         // console.log('last warm-up image');
         this.start(intervals[intervalIndex]).on('end', function() {
-          feedback('--')
+          feedback('--', timeFeedback);
           img.src = randomImage();
         });
       } else {
@@ -76,5 +79,19 @@ window.onload = function() {
     img.src = randomImage();
     timer.start(intervals[intervalIndex]);
     this.style.display = 'none';
+  }
+
+  // setup pause button
+  // ----------------------------------------
+  pauseBtn.onclick = function(e) {
+    if (paused) {
+      timer.start();
+      paused = false;
+      feedback('Pause', pauseBtn);
+    } else {
+      timer.pause();
+      paused = true;
+      feedback('Resume', pauseBtn);
+    }
   }
 }
